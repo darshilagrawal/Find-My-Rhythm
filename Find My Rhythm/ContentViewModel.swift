4dd21ab7d2233eb1fbb7a0c6b1b1bddeb7a32457
darshilagrawal <darshilagrawal2000@gmail.com>
2021-09-21 23:38:25 +0530

    Added Listen Function

--- Find My Rhythm/ContentViewModel.swift
+++ Find My Rhythm/ContentViewModel.swift
@@ -24,6 +24,39 @@ class ContentViewModel: NSObject, ObservableObject {
         super.init()
         session.delegate = self
     }
+    
+    func listeningTriggered(){
+        guard !audioEngine.isRunning else {
+            audioEngine.stop()
+            DispatchQueue.main.async {
+                self.isRecording = false
+            }
+            return
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        audioSession.requestRecordPermission { granted in
+            guard granted else { return }
+            try? audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+            let inputNode = self.audioEngine.inputNode
+            let recordingFormat = inputNode.outputFormat(forBus: 0)
+            inputNode.installTap(onBus: 0,
+                                 bufferSize: 1024,
+                                 format: recordingFormat) { (buffer: AVAudioPCMBuffer,
+                                                             when: AVAudioTime) in
+                self.session.matchStreamingBuffer(buffer, at: nil)
+            }
+            self.audioEngine.prepare()
+            do {
+                try self.audioEngine.start()
+            } catch (let error) {
+                assertionFailure(error.localizedDescription)
+            }
+            DispatchQueue.main.async {
+                self.isRecording = true
+            }
+        }
+    }
 }
 
 @available(iOS 15.0, *)
