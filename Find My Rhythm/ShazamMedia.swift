56b24f48477ec3a08b2b751091adddab06e5e6dd
darshilagrawal <darshilagrawal2000@gmail.com>
2021-09-24 23:02:33 +0530

    Added Initialiser

--- Find My Rhythm/ShazamMedia.swift
+++ Find My Rhythm/ShazamMedia.swift
@@ -13,4 +13,12 @@ struct ShazamMedia: Decodable {
     let artistName: String?
     let albumArtURL: URL?
     let genres: [String]
+    
+    init?(title:String, subtitle:String, albumArtURL: URL, genres: [String] ) {
+        self.title = title
+        self.subtitle = subtitle
+        self.artistName = artistName
+        self.albumArtURL = albumArtURL
+        self.genres = genres
+    }
 }
