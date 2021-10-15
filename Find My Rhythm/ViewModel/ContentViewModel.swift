97c9ece48f843b8c0d188810242298248e2d37c6
darshilagrawal <darshilagrawal2000@gmail.com>
2021-10-15 22:50:13 +0530

    Redundant file

--- Find My Rhythm/ViewModel/ContentViewModel.swift
+++ Find My Rhythm/ViewModel/ContentViewModel.swift
@@ -1,14 +1,11 @@
 //
-//  ContentViewModel.swift
+//  ShazamMedia.swift
 //  Find My Rhythm
 //
-//  Created by Darshil Agrawal on 22/09/21.
+//  Created by Darshil Agrawal on 20/09/21.
 //
 
 import Foundation
-import ShazamKit
-import AVKit
-import SwiftUI
 
 struct ShazamMedia: Decodable {
     let title: String?
@@ -16,6 +13,12 @@ struct ShazamMedia: Decodable {
     let artistName: String?
     let albumArtURL: URL?
     let genres: [String]
+    
+    init?(title:String, subtitle: String, albumArtURL: URL, genres: [String], artistName: String) {
+        self.title = title
+        self.subtitle = subtitle
+        self.albumArtURL = albumArtURL
+        self.genres = genres
+        self.artistName = artistName
+    }
 }
-
-
